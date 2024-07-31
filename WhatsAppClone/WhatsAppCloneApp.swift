78640120d31ea1d23188d867d753e44e1160d557
MADMAXIMUUS <maxim00nechaev@gmail.com>
2024-07-31 13:33:34 +0300

    Initial Commit

---
+++ WhatsAppClone/WhatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneApp.swift
+//  WhatsAppClone
+//
+//  Created by Максим Нечаев on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
